cd44de5ebcda34cf9a6ad78441d484b5b5b06514
Amapola <htrsanbl@gmail.com>
2025-11-30 18:21:16 -0500

    feat: The onStartup() function has been override to implements DI

--- AccessControl/App.xaml.cs
+++ AccessControl/App.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
 namespace AccessControl
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider? Services { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<MainWindow>();
+
+            Services = services.BuildServiceProvider();
+
+            base.OnStartup(e);
+
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 
 }
