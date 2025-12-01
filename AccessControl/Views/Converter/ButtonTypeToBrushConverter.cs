bed39f6b17a0b177dcde63a32ebdd71e57692258
Amapola <htrsanbl@gmail.com>
2025-12-01 04:54:34 -0500

    feat: The button type to brush/color converter were added

---
+++ AccessControl/Views/Converter/ButtonTypeToBrushConverter.cs
@@ -0,0 +1,43 @@
+﻿using AccessControl.Views.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AccessControl.Views.Converter
+{
+    public class ButtonTypeToBrushConverter : IValueConverter
+    {
+        public ButtonTypeToBrushConverter() {}
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ButtonType buttonType)
+            {
+                return buttonType switch
+                {
+                    ButtonType.Sober => GetBrush("AppBlackBrush"),
+                    ButtonType.Vibrant => GetBrush("AppBlueBrush"),
+                    ButtonType.Success => GetBrush("AppGreenBrush"),
+                    ButtonType.Danger => GetBrush("AppRedBrush"),
+
+                    _ => GetBrush("AppBlackBrush")
+                };
+            }
+
+            return GetBrush("AppBlackBrush");
+        }
+
+        private static object GetBrush(string key) 
+            => (Brush)Application.Current.FindResource(key);
+        
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AccessControl/Views/Converter/ButtonTypeToColorConverter.cs
@@ -0,0 +1,38 @@
+﻿using AccessControl.Views.Enums;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AccessControl.Views.Converter
+{
+    class ButtonTypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value is ButtonType buttonType)
+            {
+                return buttonType switch
+                {
+                    ButtonType.Sober => GetColor("AppBlackColor"),
+                    ButtonType.Vibrant => GetColor("AppBlueColor"),
+                    ButtonType.Success => GetColor("AppGreenColor"),
+                    ButtonType.Danger => GetColor("AppRedColor"),
+
+                    _ => GetColor("AppBlackColor")
+                };
+            }
+
+            return GetColor("AppBlackColor");
+        }
+
+        private static object GetColor(string key)
+           => Application.Current.FindResource(key);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
