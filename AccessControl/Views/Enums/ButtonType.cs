1e342634be9746136577443c230871cd23b9bd28
Amapola <htrsanbl@gmail.com>
2025-12-01 04:53:43 -0500

    feat: The button type enum is added

---
+++ AccessControl/Views/Enums/ButtonType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Views.Enums
+{
+    public enum ButtonType
+    {
+        Sober,
+        Vibrant,
+        Success,
+        Danger
+    }
+}
