b0c862c7a90e277c43cf19aa2178734412846fe0
Amapola <htrsanbl@gmail.com>
2025-12-01 19:54:21 -0500

    feat: includes the dtos for sp

---
+++ AccessControl/Infraestructure/Dto/LoginResultDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Infraestructure.Dto
+{
+    public class LoginResultDto
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? FullName { get; set; }
+
+        public int EstablishmentId { get; set; }
+        public string? Role { get; set; }
+        public bool IsActive { get; set; }
+        public string? IdentityDocument { get; set; }
+        public string? PhoneNumber { get; set; }
+        public bool MustChangePassword { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+
+        // Sp returns this
+        public string? EstablishmentName { get; set; }
+        public int? MaxCapacity { get; set; }
+    }
+
+}
