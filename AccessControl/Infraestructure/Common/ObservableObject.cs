cfcabe555fe4215a834583e073cf61db0191d3d7
Amapola <htrsanbl@gmail.com>
2025-12-01 22:56:27 -0500

    feat: Core infrastructure object were are added

---
+++ AccessControl/Infraestructure/Common/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace AccessControl.Infraestructure.Common
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetProperty<T>(
+            ref T storage,
+            T value,
+            [System.Runtime.CompilerServices.CallerMemberName] string propertyName = ""
+        )
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
