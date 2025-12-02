a6304201d060f8923aabb92f92c7a446adcae002
Amapola <htrsanbl@gmail.com>
2025-12-02 03:46:26 -0500

    feat: Includes the AsyncRelayCommand to send secured commands in threads

---
+++ AccessControl/Infraestructure/Common/AsyncRelayCommand.cs
@@ -0,0 +1,45 @@
+using System.Windows.Input;
+
+namespace AccessControl.Infraestructure.Common
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<object?, Task> _execute;
+        private readonly Predicate<object?>? _canExecute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<object?, Task> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return !_isExecuting && (_canExecute == null || _canExecute(parameter));
+        }
+
+        public async void Execute(object? parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                try
+                {
+                    _isExecuting = true;
+                    RaiseCanExecuteChanged();
+                    await _execute(parameter);
+                }
+                finally
+                {
+                    _isExecuting = false;
+                    RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        public void RaiseCanExecuteChanged() =>
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
