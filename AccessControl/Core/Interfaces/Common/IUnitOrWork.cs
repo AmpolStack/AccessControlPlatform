cf74eaef7978ce3568163ba93c84787b778fcc7d
Amapola <htrsanbl@gmail.com>
2025-12-01 21:30:39 -0500

    feat: The uow was added

---
+++ AccessControl/Core/Interfaces/Common/IUnitOrWork.cs
@@ -0,0 +1,14 @@
+﻿
+using AccessControl.Core.Models;
+
+namespace AccessControl.Core.Interfaces.Common
+{
+    public interface IUnitOrWork
+    {
+        public Task BeginAsync();
+        public Task CommitAsync();
+        public Task RollbackAsync();
+
+        AccessControlContext Context { get; }
+    }
+}
---
+++ AccessControl/Infraestructure/Common/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using AccessControl.Core.Interfaces.Common;
+using AccessControl.Core.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace AccessControl.Infraestructure.Common
+{
+    public class UnitOfWork : IUnitOrWork
+    {
+        private readonly AccessControlContext _context;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(AccessControlContext context)
+        {
+            _context = context;
+        }
+
+        public AccessControlContext Context => _context;
+
+        public async Task BeginAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public Task CommitAsync() => _transaction!.CommitAsync();
+        public Task RollbackAsync() => _transaction!.RollbackAsync();
+    }
+}
