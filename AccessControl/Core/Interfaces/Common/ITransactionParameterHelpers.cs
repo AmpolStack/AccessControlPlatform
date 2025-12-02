24df3d4aea00f66af47ba58ed0859229d24ddf11
Amapola <htrsanbl@gmail.com>
2025-12-01 21:32:08 -0500

    feat: The transactionParameterHelper implementation and definition were are included

---
+++ AccessControl/Core/Interfaces/Common/ITransactionParameterHelpers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Core.Interfaces.Common
+{
+    public interface ITransactionParameterHelpers
+    {
+        SqlParameter OutBit(string name);
+
+        SqlParameter OutString(string name, int size);
+
+        (bool Success, string Message) ReadOutput(SqlParameter result, SqlParameter message);
+    }
+}
---
+++ AccessControl/Infraestructure/Common/TransactionParameterHelpers.cs
@@ -0,0 +1,38 @@
+﻿using AccessControl.Core.Interfaces.Common;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessControl.Infraestructure.Common
+{
+    public class TransactionParameterHelpers : ITransactionParameterHelpers
+    {
+        public SqlParameter OutBit(string name)
+        {
+            return new SqlParameter(name, SqlDbType.Bit)
+            {
+                Direction = ParameterDirection.Output
+            };
+        }
+
+        public SqlParameter OutString(string name, int size)
+        {
+            return new SqlParameter(name, SqlDbType.NVarChar, size)
+            {
+                Direction = ParameterDirection.Output
+            };
+        }
+
+        public (bool Success, string Message) ReadOutput(SqlParameter result, SqlParameter message)
+        {
+            return (
+                Convert.ToBoolean(result.Value),
+                Convert.ToString(message.Value) ?? string.Empty
+            );
+        }
+    }
+}
